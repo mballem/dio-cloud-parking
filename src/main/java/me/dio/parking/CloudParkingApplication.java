5c78e5f77513ac37d75fc22a4859ed8a12a4cb85
Ballem <romarcio@gmail.com>
2022-08-11 18:47:48 -0300

    Adicionado Locale e Timezone brasileiros

--- src/main/java/me/dio/parking/CloudParkingApplication.java
+++ src/main/java/me/dio/parking/CloudParkingApplication.java
@@ -3,8 +3,17 @@ package me.dio.parking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class CloudParkingApplication {
+
+	@PostConstruct
+	public void init(){
+		// Setting Spring Boot SetTimeZone
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudParkingApplication.class, args);
