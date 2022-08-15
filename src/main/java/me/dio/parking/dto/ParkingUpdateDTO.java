97e4b92d6d5ccdb1b5a12a0a4f9f4cd7b72d0ab1
Ballem <romarcio@gmail.com>
2022-08-15 14:36:48 -0300

    Adicionado DTO para acao de update com validacao de campos

---
+++ src/main/java/me/dio/parking/dto/ParkingUpdateDTO.java
@@ -0,0 +1,47 @@
+package me.dio.parking.dto;
+
+import javax.validation.constraints.Size;
+
+public class ParkingUpdateDTO {
+
+    @Size(min = 8, max = 8)
+    private String license;
+    @Size(min = 2, max = 2)
+    private String state;
+    @Size(min = 2, max = 50)
+    private String model;
+    @Size(min = 3, max = 50)
+    private String color;
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
