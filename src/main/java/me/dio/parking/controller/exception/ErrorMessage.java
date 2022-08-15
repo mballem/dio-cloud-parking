37f60e0746ef9dd4650c9ad85a1a4eeff6606f91
Ballem <romarcio@gmail.com>
2022-08-15 15:39:40 -0300

    Adicionadas classes para tratar mensagem de erro de validacao baseada em BadRequest

---
+++ src/main/java/me/dio/parking/controller/exception/ErrorMessage.java
@@ -0,0 +1,40 @@
+package me.dio.parking.controller.exception;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+class ErrorMessage {
+
+    private String uri;
+    private String method;
+    private BindingResult result;
+    private Map<String, String> errors = new HashMap<>();
+
+    public ErrorMessage(HttpServletRequest request, BindingResult result) {
+        this.uri = request.getRequestURI();
+        this.method = request.getMethod();
+        addError(result);
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public Map<String, String> getErrors() {
+        return errors;
+    }
+
+    private void addError(BindingResult result) {
+        for (FieldError fieldError : result.getFieldErrors()) {
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+    }
+}
---
+++ src/main/java/me/dio/parking/controller/exception/ParkingRequestException.java
@@ -0,0 +1,24 @@
+package me.dio.parking.controller.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ParkingRequestException {
+
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    public ResponseEntity<ErrorMessage> badRequestException(MethodArgumentNotValidException exm,
+                                                       HttpServletRequest request, BindingResult result) {
+
+        return ResponseEntity.badRequest().body(new ErrorMessage(request, result));
+    }
+}
+
